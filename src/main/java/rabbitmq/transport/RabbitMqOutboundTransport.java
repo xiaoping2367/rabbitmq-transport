43b4900953f195ed6dc01ff91d7e21184247d12d
Brian <blocke@odf.state.or.us>
2014-03-25 11:23:51 -0700

    OutboundTransfer

--- src/main/java/rabbitmq/transport/RabbitMqOutboundTransport.java
+++ src/main/java/rabbitmq/transport/RabbitMqOutboundTransport.java
@@ -10,11 +10,27 @@ import com.esri.ges.core.component.RunningException;
 import com.esri.ges.core.component.RunningState;
 import com.esri.ges.transport.OutboundTransportBase;
 import com.esri.ges.transport.TransportDefinition;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
 
 public class RabbitMqOutboundTransport extends OutboundTransportBase
 {
   static final private Log log = LogFactory.getLog(RabbitMqOutboundTransport.class);
 
+  private String hostname;
+  private String userName;
+  private String password;
+  private String queueName;
+
+private String exchange;
+  private String exchangeType;
+  private String routingKey;
+  
+  private ConnectionFactory connFactory;
+  private Connection conn;
+  private Channel model;
+    
   public RabbitMqOutboundTransport(TransportDefinition definition) throws ComponentException
   {
     super(definition);
@@ -23,41 +39,76 @@ public class RabbitMqOutboundTransport extends OutboundTransportBase
   @SuppressWarnings("unused")
   private void applyProperties() throws IOException
   {
-    // This method shows how to extract properties that were set by your user
-    boolean myBooleanProperty = (Boolean) getProperty("broadcast").getValue();
-    String myStringProperty = getProperty("host").getValueAsString();
+    hostname = getProperty("hostname").getValueAsString();
+    userName = getProperty("userName").getValueAsString();
+    password = getProperty("password").getValueAsString();
+    exchange = getProperty("exchange").getValueAsString();
+    exchangeType = getProperty("exchangeType").getValueAsString();
+    queueName = getProperty("queueName").getValueAsString();
+    routingKey = getProperty("routingKey").getValueAsString();
   }
 
   @SuppressWarnings("unused")
   public void receive(ByteBuffer buffer, String channelId)
   {
-    // This function is called whenever a GeoEvent is processed by an adapter,
-    // and translated into raw bytes.
-    // As an author, you are responsible for reading from the buffer to remove
-    // data from it. Any data left in
-    // The buffer will be left there for you to read again the next time this
-    // function is called. This allows you
-    // to buffer up larger amounts of data before sending them (e.g. filling up
-    // a socket packet before sending it).
-    byte b = 0;
-    while (buffer.hasRemaining())
-      b = buffer.get();
+	byte[] b = new byte[buffer.remaining()];
+    try
+    {
+		model.basicPublish(exchange, routingKey, null, b);
+	} catch (IOException e)
+	{
+		log.error("Cannot Publish to RabbitMQ: " + e);
+	}
   }
-
+  
+ 
   public void start() throws RunningException
   {
     try
     {
       setRunningState(RunningState.STARTING);
       applyProperties();
-      // As a transport author, this is where you would add code to create
-      // resources, start threads, etc.
+      connFactory = new ConnectionFactory();
+      connFactory.setHost(hostname);
+      connFactory.setUsername(userName);
+      connFactory.setPassword(password);
+      conn = connFactory.newConnection();
+      model = conn.createChannel();
+      
+      //QueueDeclare and ExchangeDeclare are Idempotent
+      
+      exchange = exchange+"."+exchangeType;
+      model.exchangeDeclare(exchange, exchangeType);
+      
+      if(queueName!="")
+      {
+    	  model.queueDeclare(queueName, false, false, false,null);
+      }
+      else
+      {
+    	queueName = model.queueDeclare().getQueue();  
+      }
+      
+      model.queueBind(queueName, exchange, routingKey);
+            
       setRunningState(RunningState.STARTED);
     }
     catch (IOException e)
     {
       log.error("Unable to initialize the " + this.getClass().getName() + " transport", e);
       setRunningState(RunningState.ERROR);
+      
+      try
+      {
+    	    if(model.isOpen())
+    	    	  model.close();
+    	      if(conn.isOpen())
+    	    	  conn.close();
+      }
+      catch(Exception rabbErr)
+      {
+    	 log.error("Possible Hung RabbitMQ Connection: "+rabbErr);
+      }
     }
   }
 }
